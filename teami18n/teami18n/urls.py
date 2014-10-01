b80834768332ca8cdfeb813d76400b066a3b045e
Rebecca Meritz <rebecca@meritz.com>
2014-10-01 22:12:22 +0200

    Add real view

--- teami18n/teami18n/urls.py
+++ teami18n/teami18n/urls.py
@@ -1,26 +1,14 @@
-from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
+from django.conf.urls import patterns, url
 from django.views.static import serve
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+from .views import HomeView
 
-urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+urlpatterns = patterns(
+    '',
+    url(r'^$', HomeView.as_view(), name="home"),
 
-    # Examples:
-    # url(r'^$', 'teami18n.views.home', name='home'),
-    # url(r'^teami18n/', include('teami18n.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-    
     # Static files
     url(r'^static/(?P<path>.*)$', serve,
-        kwargs={'document_root':settings.STATIC_ROOT})
+        kwargs={'document_root': settings.STATIC_ROOT})
 )
