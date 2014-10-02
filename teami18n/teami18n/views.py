b5bc3f98f78e76f5fc59d21552f48b7a74148fed
Rebecca Meritz <rebecca@meritz.com>
2014-10-02 23:47:25 +0200

    Test having a map

--- teami18n/teami18n/views.py
+++ teami18n/teami18n/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import TemplateView
 
+# Countries Data:
+# https://github.com/SmileyChris/django-countries/blob/master/django_countries/data.py#L45
+
 
 class HomeView(TemplateView):
     template_name = "base.html"
