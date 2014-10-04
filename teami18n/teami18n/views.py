e2268fd7337995ac02841479934eab648580f77a
Ana Hristova <ana.nikolaeva@gmail.com>
2014-10-04 17:26:55 +0200

    Colour countries by population

--- teami18n/teami18n/views.py
+++ teami18n/teami18n/views.py
@@ -1,4 +1,7 @@
+import json
 from django.views.generic import TemplateView
+
+from .models import Country
 
 # Countries Data:
 # https://github.com/SmileyChris/django-countries/blob/master/django_countries/data.py#L45
@@ -6,3 +9,10 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "base.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        countries = Country.objects.filter(population__isnull=False)
+        country_list = [(c.code, c.population) for c in countries]
+        context["population_data"] = json.dumps(dict(country_list))
+        return context
